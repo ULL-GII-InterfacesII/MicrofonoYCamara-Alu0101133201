32b284440f067e67eb57f2da03ffec461f6a2b0f
alu0101133201 <50806571+alu0101133201@users.noreply.github.com>
2020-11-22 21:27:49 +0000

    Scripts añadidos

---
+++ scripts/microphone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class microphone : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+      audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown("space")) {
+        if (!Microphone.IsRecording(""))
+          audioSource.clip = Microphone.Start("", false, 15, 48000);
+        else {
+          Microphone.End("");
+          audioSource.Play();
+        }
+      }
+    }
+}
---
+++ scripts/webCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class webCamera : MonoBehaviour
+{
+    Renderer renderer;
+    WebCamTexture webcamTexture;
+    Texture defaultTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+      renderer = GetComponent<Renderer>();
+      defaultTexture = renderer.material.mainTexture;
+      webcamTexture = new WebCamTexture("HD WebCam");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey("v")) {
+        if (!webcamTexture.isPlaying) {
+          renderer.material.mainTexture = webcamTexture;
+          webcamTexture.Play();
+        } else {
+          webcamTexture.Stop();
+          renderer.material.mainTexture = defaultTexture;
+      }
+    }
+  }
+
+};
